9f6012743bfbb24de100f2ffce66aef8ddbb9fd8
Thommzy <tomiwaobeisun@yahoo.com>
2020-02-05 14:34:43 +0100

    Installed my React app and Projects Begins Now

---
+++ mainapp/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">FrontEnd Mint Begin</div>;
+}
+
+export default App;
